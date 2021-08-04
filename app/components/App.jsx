dde8e419deced98e7853f1bb0ea0d2f969d83cf8
VoN <sudo.von.contact@gmail.com>
2021-08-04 03:44:54 -0500

    feat: :sparkles: Finish button to delete notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -22,7 +22,7 @@ export default class App extends React.Component {
         return (
             <div>
                 <button onClick={this.addNote}>+</button>
-                <Notes notes={notes} />
+                <Notes notes={notes} onDelete={this.deleteNote}/>
             </div>
         )
     }
@@ -32,6 +32,12 @@ export default class App extends React.Component {
                 id: uuid.v4(),
                 task: 'New task'
             }])
+        })
+    }
+    deleteNote = (id, e) => {
+        e.stopPropagation()
+        this.setState({
+            notes: this.state.notes.filter(note => note.id !== id)
         })
     }
 }
