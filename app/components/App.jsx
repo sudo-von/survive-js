2d31747ace30246b132dc733a089f97a6a055b21
VoN <sudo.von.contact@gmail.com>
2021-08-04 03:24:52 -0500

    feat: :sparkles: Add data to the principal component and pass data to the other components via props

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import Notes from './Notes'
+import uuid from 'uuid'
+
+const notes = [
+    {
+        id: uuid.v4(),
+        task: 'Learn React'
+    },
+    {
+        id: uuid.v4(),
+        task: 'Do laundry'
+    }
+]
 
 export default () =>
     <div>
         <button onClick={() => console.log('añadir nota')}>+</button>
-        <Notes />
+        <Notes notes={notes}/>
     </div>
