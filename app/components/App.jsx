f477f3db0619fe965f6380cfc6240bed01ebde7f
VoN <sudo.von.contact@gmail.com>
2021-08-04 03:29:58 -0500

    feat: :sparkles: Ussing class and a state instead of functional component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,19 +2,28 @@ import React from 'react'
 import Notes from './Notes'
 import uuid from 'uuid'
 
-const notes = [
-    {
-        id: uuid.v4(),
-        task: 'Learn React'
-    },
-    {
-        id: uuid.v4(),
-        task: 'Do laundry'
+export default class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            notes: [{
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Do laundry'
+                }
+            ]
+        }
     }
-]
-
-export default () =>
-    <div>
-        <button onClick={() => console.log('añadir nota')}>+</button>
-        <Notes notes={notes}/>
-    </div>
+    render() {
+        const { notes } = this.state
+        return (
+            <div>
+                <button onClick={() => console.log('add note')}>+</button>
+                <Notes notes={notes} />
+            </div>
+        )
+    }
+}
